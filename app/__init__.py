57388e8bee1898397885f7cca59ee10a0ef8a29c
jhonifigueredo <jhoni_choc@hotmail.com>
2020-03-02 20:35:30 -0400

    Aula 02

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 
 @app.route('/home')
 def home():
+    title = "Atividades Complementares"
     e = ['SINPRAD', 'Semana ADS', 'Semana de Ciência e Tecnologia']
-    return render_template("index.html", eventos=e)
+    return render_template("index.html", eventos=e, titulo=title)
